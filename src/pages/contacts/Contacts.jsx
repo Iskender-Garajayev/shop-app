8e6ab22b8c0cda00fe09aa4a85e10b61952d3e8a
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-04-28 04:00:19 +0300

    contacts

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -6,6 +6,27 @@ const Contacts = () => {
   return (
     <div>
       <Header/>
+        <div className="contacts-container">
+
+          <div className="call-write">
+            <div className="call">
+              <p>call to us</p>
+              <p>We are available 24/7, 7 days a week.</p>
+              <p>Phone: +8801611112222</p>
+            </div>
+
+            <div className="write">
+              <p>Write To US</p>
+              <p>Fill out our form and we will contact you within 24 hours.</p>
+              <p>Emails: customer@exclusive.com</p>
+              <p>Emails: support@exclusive.com</p>
+            </div>
+          </div>
+
+          <div className="send-email">
+            
+          </div>
+        </div>
       <Footer/>
     </div>
   )
