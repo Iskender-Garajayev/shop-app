ac209b2b0e85574dc247c5bb0d4dd2a390c41d47
Iskender-Garajayev <yoloten.isa@gmail.com>
2023-05-08 01:51:45 +0300

    contact component

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/footer/Footer'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/footer/Footer";
+import './contact.css'
 
 const Contacts = () => {
   return (
     <div>
-      <Header/>
-        <div className="contacts-container">
+      <Header />
+      <div className="contacts-container">
 
           <div className="call-write">
             <div className="call">
-              <p>call to us</p>
+              <p> 
+                <span className="contact-icon">
+                  
+                <ion-icon name="call-outline"></ion-icon> 
+                </span>
+                call to us</p>
               <p>We are available 24/7, 7 days a week.</p>
-              <p>Phone: +8801611112222</p>
+              <p>Phone:<a href="tel:+905451522343">+905451522343</a> </p>
             </div>
 
             <div className="write">
-              <p>Write To US</p>
+              <div> 
+                <span className="contact-icon">
+                <ion-icon name="mail-outline"></ion-icon>
+                </span> 
+                Write To US </div>
               <p>Fill out our form and we will contact you within 24 hours.</p>
               <p>Emails: customer@exclusive.com</p>
               <p>Emails: support@exclusive.com</p>
             </div>
           </div>
 
-          <div className="send-email">
-            
-          </div>
-        </div>
-      <Footer/>
-    </div>
-  )
-}
 
-export default Contacts
+        <div className="email-container">
+
+
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Contacts;
