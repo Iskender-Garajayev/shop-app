61fbf005a6a1fc6fdc4aadf8a041a5c752fd5a1c
Iskender Garajayev <yoloten.isa@gmail.com>
2025-02-08 07:06:37 +0500

    update

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/footer/Footer'
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/footer/Footer';
 
 const Cart = () => {
   return (
     <div>
-      <Header/>
+      <Header />
       Cart
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
